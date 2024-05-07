ee346c64862b7df8735f869d0f4ae1361c1f3573
Airme1 <emmanuelochoja@yahoo.com>
2024-05-07 10:34:16 +0100

    task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import holberton from "./holberton_logo.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr></hr>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <hr></hr>
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
